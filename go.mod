2f19a1bdd973481b98e3ab3c12cfced07ef0e546
chanxuehong <chanxuehong@gmail.com>
2020-11-10 16:21:27 +0800

    fix: fix data race

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/chanxuehong/rand
+
+go 1.15
--- rand.go
+++ rand.go
@@ -6,6 +6,7 @@ import (
 	"sync"
 	"sync/atomic"
 	"time"
+	"unsafe"
 )
 
 const (
@@ -14,12 +15,29 @@ const (
 )
 
 var (
-	gSalt     = make([]byte, saltLen)
-	gSequence = Uint32()
-
-	gMutex                   sync.Mutex
-	gSaltLastUpdateTimestamp int64 = -saltUpdateInterval
+	_mutex                   sync.Mutex
+	_salt                    unsafe.Pointer
+	_saltLastUpdateTimestamp int64 = -saltUpdateInterval
+	_sequence                      = Uint32()
 )
+
+func init() {
+	salt := make([]byte, saltLen)
+	Read(salt)
+	storeSalt(salt)
+}
+
+func storeSalt(salt []byte) {
+	atomic.StorePointer(&_salt, unsafe.Pointer(&salt))
+}
+
+func loadSalt() []byte {
+	p := atomic.LoadPointer(&_salt)
+	if p == nil {
+		return nil
+	}
+	return *(*[]byte)(p)
+}
 
 // New returns 16-byte raw random bytes.
 // It is not printable, you can use encoding/hex or encoding/base64 to print it.
@@ -27,21 +45,22 @@ func New() (rd [16]byte) {
 	timeNow := time.Now()
 	timeNowUnix := timeNow.Unix()
 
-	if timeNowUnix >= atomic.LoadInt64(&gSaltLastUpdateTimestamp)+saltUpdateInterval {
-		gMutex.Lock() // Lock
-		if timeNowUnix >= gSaltLastUpdateTimestamp+saltUpdateInterval {
-			gSaltLastUpdateTimestamp = timeNowUnix
-			gMutex.Unlock() // Unlock
-
-			Read(gSalt)
-			copy(rd[:], gSalt)
+	if timeNowUnix >= atomic.LoadInt64(&_saltLastUpdateTimestamp)+saltUpdateInterval {
+		_mutex.Lock() // Lock
+		if timeNowUnix >= atomic.LoadInt64(&_saltLastUpdateTimestamp)+saltUpdateInterval {
+			salt := make([]byte, saltLen)
+			Read(salt)
+			storeSalt(salt)
+			atomic.StoreInt64(&_saltLastUpdateTimestamp, timeNowUnix)
+			copy(rd[:], salt)
+			_mutex.Unlock() // Unlock
 			return
 		}
-		gMutex.Unlock() // Unlock
+		_mutex.Unlock() // Unlock
 	}
-	sequence := atomic.AddUint32(&gSequence, 1)
 
 	timeNowUnixNano := timeNow.UnixNano()
+	sequence := atomic.AddUint32(&_sequence, 1)
 	var src [6 + 4 + saltLen]byte // 6+4+45==55
 	src[0] = byte(timeNowUnixNano >> 40)
 	src[1] = byte(timeNowUnixNano >> 32)
@@ -53,7 +72,7 @@ func New() (rd [16]byte) {
 	src[7] = byte(sequence >> 16)
 	src[8] = byte(sequence >> 8)
 	src[9] = byte(sequence)
-	copy(src[10:], gSalt)
+	copy(src[10:], loadSalt())
 
 	return md5.Sum(src[:])
 }
